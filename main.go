65d32067969111140c57c2be519f9b4122e5ce6b
Edward <edward@schaumanhaigh.com>
2021-05-19 21:28:31 +0100

    Add calculate scores - WIP, broken

--- main.go
+++ main.go
@@ -20,6 +20,29 @@ type Card struct {
 	Rank string
 }
 
+type Suit struct {
+	Hearts   string
+	Diamonds string
+	Clubs    string
+	Spades   string
+}
+
+type Rank struct {
+	Two   string
+	Three string
+	Four  string
+	Five  string
+	Six   string
+	Seven string
+	Eight string
+	Nine  string
+	Ten   string
+	Jack  string
+	Queen string
+	King  string
+	Ace   string
+}
+
 func shuffleDeck() Deck {
 	deck := Deck{
 		[]Card{
@@ -132,8 +155,163 @@ func (deck *Deck) getTheRiver() (Card, Deck) {
 	return fifth, newDeck
 }
 
-func calculateScore(hand Hand, communityCards []Card) int {
-	return 0
+type Score struct {
+	RoyalFlush    bool
+	StraightFlush bool
+	FourOfAKind   bool
+	FullHouse     bool
+	Flush         bool
+	Straight      bool
+	ThreeOfAKind  bool
+	TwoPair       bool
+	Pair          bool
+	HighCard      Card
+}
+
+func (score *Score) highCard(hand Hand, communityCards []Card) Card {
+	cardRanks := []string{}
+	for _, card := range communityCards {
+		// Ace is best, early return :)
+		if card.getRank() == "Ace" {
+			return card
+		}
+		cardRanks = append(cardRanks, card.getRank())
+	}
+
+	return Card{"Hearts", "Ace"}
+}
+
+func (score *Score) pair(hand Hand, communityCards []Card) bool {
+	cardRanks := []string{}
+	for _, card := range communityCards {
+		cardRanks = append(cardRanks, card.getRank())
+	}
+
+	return true
+}
+
+func (score *Score) twoPairs(hand Hand, communityCards []Card) bool {
+	cardRanks := []string{}
+	for _, card := range communityCards {
+		cardRanks = append(cardRanks, card.getRank())
+	}
+
+	return true
+}
+
+func (score *Score) threeOfAKind(hand Hand, communityCards []Card) bool {
+	cardRanks := []string{}
+	for _, card := range communityCards {
+		cardRanks = append(cardRanks, card.getRank())
+	}
+
+	return true
+}
+
+func (score *Score) straight(hand Hand, communityCards []Card) bool {
+	cardRanks := []string{}
+	for _, card := range communityCards {
+		cardRanks = append(cardRanks, card.getRank())
+	}
+
+	return true
+}
+
+func (score *Score) flush(hand Hand, communityCards []Card) bool {
+	cardRanks := []string{}
+	for _, card := range communityCards {
+		cardRanks = append(cardRanks, card.getRank())
+	}
+
+	return true
+}
+
+func (score *Score) fullHouse(hand Hand, communityCards []Card) bool {
+	cardRanks := []string{}
+	for _, card := range communityCards {
+		cardRanks = append(cardRanks, card.getRank())
+	}
+
+	return true
+}
+
+func (score *Score) fourOfAKind(hand Hand, communityCards []Card) bool {
+	cardRanks := []string{}
+	for _, card := range communityCards {
+		cardRanks = append(cardRanks, card.getRank())
+	}
+
+	return true
+}
+
+func (score *Score) straightFlush(hand Hand, communityCards []Card) bool {
+	cardRanks := []string{}
+	for _, card := range communityCards {
+		cardRanks = append(cardRanks, card.getRank())
+	}
+
+	return true
+}
+
+func (score *Score) royalFlush(hand Hand, communityCards []Card) bool {
+	cardRanks := []string{}
+	for _, card := range communityCards {
+		cardRanks = append(cardRanks, card.getRank())
+	}
+
+	return true
+}
+
+func calculateScore(hand Hand, communityCards []Card) (int, Card) {
+	score := Score{}
+
+	// Royal Flush?
+	if score.royalFlush(hand, communityCards) {
+		return 10, score.highCard(hand, communityCards)
+	}
+
+	// Straight Flush?
+	if score.straightFlush(hand, communityCards) {
+		return 9, score.highCard(hand, communityCards)
+	}
+
+	// Four of a Kind?
+	if score.fourOfAKind(hand, communityCards) {
+		return 8, score.highCard(hand, communityCards)
+	}
+
+	// Full House?
+	if score.fullHouse(hand, communityCards) {
+		return 7, score.highCard(hand, communityCards)
+	}
+
+	// Flush?
+	if score.flush(hand, communityCards) {
+		return 6, score.highCard(hand, communityCards)
+	}
+
+	// Straight?
+	if score.straight(hand, communityCards) {
+		return 5, score.highCard(hand, communityCards)
+	}
+
+	// Three of a Kind?
+	if score.threeOfAKind(hand, communityCards) {
+		return 4, score.highCard(hand, communityCards)
+	}
+
+	// Two Pair?
+	if score.twoPairs(hand, communityCards) {
+		return 3, score.highCard(hand, communityCards)
+	}
+
+	// Pair?
+	if score.pair(hand, communityCards) {
+		return 2, score.highCard(hand, communityCards)
+	}
+
+	// High Card?
+	return 1, score.highCard(hand, communityCards)
 }
 
 func playRound() {
@@ -157,8 +335,8 @@ func playRound() {
 	communityCards = append(communityCards, fifth)
 
 	// calculate winner
-	playerOneScore := calculateScore(playerOneHand, communityCards)
-	playerTwoScore := calculateScore(playerTwoHand, communityCards)
+	playerOneScore, playerOneHighCard := calculateScore(playerOneHand, communityCards)
+	playerTwoScore, playerTwoHighCard := calculateScore(playerTwoHand, communityCards)
 
 	if playerOneScore > playerTwoScore {
 		fmt.Println("Player One Won")
@@ -166,7 +344,11 @@ func playRound() {
 	if playerTwoScore > playerOneScore {
 		fmt.Println("Player Two Won")
 	} else {
-		fmt.Println("Draw!")
+		if playerOneHighCard > playerTwoHighCard {
+			fmt.Println("Player One Won")
+		} else {
+			fmt.Println("Player Two Won")
+		}
 	}
 }
 
